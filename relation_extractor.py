d56096b5c1fb2c09fcb6f5816ebf5e8bf188c82f
Rakesh Choudhary <51029292+rakeshcarpediem@users.noreply.github.com>
2021-07-21 13:57:28 +0000

    Update relation_extractor.py

--- relation_extractor.py
+++ relation_extractor.py
@@ -7,10 +7,10 @@ def Stanford_Relation_Extractor():
 
     
     print('Relation Extraction Started')
-
+    current_directory = os.getcwd()
     for f in glob.glob(os.getcwd() + "/data/output/kg/*.txt"):        
         print("Extracting relations for " + f.split("/")[-1])
-        current_directory = os.getcwd()
+        
         os.chdir(current_directory + '/stanford-openie')
 
         p = subprocess.Popen(['./process_large_corpus.sh',f,f + '-out.csv'], stdout=subprocess.PIPE)
